d62c7d14e1dad0ccfe27a43eb87e29b52a6c8a14
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-09-12 20:48:22 +0300

    added brands and env

--- src/app/features/categories/categories.component.ts
+++ src/app/features/categories/categories.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CategoriesService } from '../../core/services/categories.service';
+import { Category } from '../../core/models/category';
 
 @Component({
   selector: 'app-categories',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit{
+  private readonly categoriesService = inject(CategoriesService);
+  categories:Category[] = [];
 
+  ngOnInit(): void {
+    this.categoriesService.getAllCategories().subscribe({
+      next:(res)=>{
+        console.log(res);
+        this.categories = res.data;
+      },
+      error:(err)=>{
+        console.log(err);
+      }
+    })
+  }
 }
