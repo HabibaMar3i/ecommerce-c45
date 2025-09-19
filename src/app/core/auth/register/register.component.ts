d07962640ea8857326b3f9c22c5da9179dd99b16
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-09-19 21:41:35 +0300

    added registration page design and the form

--- src/app/core/auth/register/register.component.ts
+++ src/app/core/auth/register/register.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.pattern(/^(010|011|012|015)[0-9]{8}$/)]),
+    rePassword: new FormControl(null, [Validators.required, Validators.pattern(/^(010|011|012|015)[0-9]{8}$/)]),
+    phone: new FormControl(null, [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).+$/)])
+  }) 
 
+  submitForm(){
+    console.log('====================================');
+    console.log(this.registerForm.value);
+    console.log(this.registerForm);
+    console.log('====================================');
+  }
 }
