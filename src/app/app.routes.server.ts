ec7507c7ea96807bc6d7794aa97d26947409586a
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-09-19 23:51:39 +0300

    fixed parameter for CSR

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'productdetails/:slug/:id',
+    renderMode: RenderMode.Client  // Using client-side rendering for product details since it has parameters
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
