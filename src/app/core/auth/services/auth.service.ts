ad024d9e679a36ebd2722c12bfbae8a197d2fa82
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-09-27 17:56:57 +0300

    added sign out in auth service

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -2,12 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../../environments/environment.development';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private readonly _httpClient = inject(HttpClient)
+  private readonly cookieService = inject(CookieService)
+  private readonly router = inject(Router)
+
 
   signUp(registerData: {}):Observable<any>{
     return this._httpClient.post(environment.baseURL + 'auth/signup', registerData)
@@ -15,5 +21,10 @@ export class AuthService {
 
   signIn(loginData: {}):Observable<any>{
     return this._httpClient.post(environment.baseURL + 'auth/signin', loginData)
+  }
+
+  signOut():void{
+    this.cookieService.delete('token')
+    this.router.navigate(['/login'])
   }
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { AuthService } from '../../../core/auth/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,10 +9,13 @@ import { initFlowbite } from 'flowbite';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent implements OnInit{
+export class NavbarComponent implements OnInit {
+  private readonly authService = inject(AuthService)
   @Input() isLogged!: boolean;
   ngOnInit(): void {
-      initFlowbite();
-    }
-
+    initFlowbite();
+  }
+  logOut() {
+    this.authService.signOut()
+  }
 }
