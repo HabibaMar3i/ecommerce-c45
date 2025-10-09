7eb40d521e5095346b0c0e39fb9ce35faac5cd8d
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-10-10 02:17:06 +0300

    added date pipe (built-in)

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,16 +2,18 @@ import { Component, inject, Input, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { AuthService } from '../../../core/auth/services/auth.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, DatePipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent implements OnInit {
   private readonly authService = inject(AuthService)
   @Input() isLogged!: boolean;
+  todayDate = new Date
   ngOnInit(): void {
     initFlowbite();
   }
