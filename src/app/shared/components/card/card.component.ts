5b3ee9167b0a6c029f8e2be048b575bee1607c33
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-10-10 02:45:40 +0300

    added onSale and trimText pipes (custom)

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -2,10 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Product } from '../../../core/models/product';
 import { RouterLink } from '@angular/router';
 import { CurrencyPipe } from '@angular/common';
+import { OnSalePipe } from '../../pipes/on-sale-pipe';
+import { TrimTextPipe } from '../../pipes/trim-text-pipe';
 
 @Component({
   selector: 'app-card',
-  imports: [RouterLink, CurrencyPipe],
+  imports: [RouterLink, CurrencyPipe, OnSalePipe, TrimTextPipe],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
