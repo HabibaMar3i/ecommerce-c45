0e0e975069709f6f86975242d25ef76db4077dce
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-10-10 02:11:27 +0300

    added currency pipe (built-in)

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../../core/models/product';
 import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [RouterLink],
+  imports: [RouterLink, CurrencyPipe],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
