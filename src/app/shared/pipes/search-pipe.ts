1af2ca2ec28bfd9c20ac5ecbf82efff729c3e943
HabibaMar3i <habibahassanmar3i@gmail.com>
2025-10-10 03:18:26 +0300

    added search pipe (custom)

--- src/app/shared/pipes/search-pipe.ts
+++ src/app/shared/pipes/search-pipe.ts
@@ -5,8 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(arr: any[], search:string): object[] {
+    return arr.filter((item)=>item.title?.toLowerCase().includes(search.toLowerCase()));
   }
-
 }
